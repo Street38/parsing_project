5a89534792f0d20da33ddb8052e0c16ba7d2b88b
Igor <irstreet08@gmail.com>
2022-08-16 11:09:02 +0800

    'first_commit'

---
+++ parsing/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponseNotFound
+
+def home(request):
+    return render(request, 'parsing/home.html')
+
+def pageNotFound(request, exception=None):
+    return HttpResponseNotFound('<h1>Страница не существует</h1>')
